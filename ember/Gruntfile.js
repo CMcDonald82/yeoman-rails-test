93114d90e952fd86491a893870715f22b341eaac
Chris <chris.mcdonald82@gmail.com>
2013-09-11 10:04:36 -0400

    update Gemfile.lock for Heroku

--- ember/Gruntfile.js
+++ ember/Gruntfile.js
@@ -22,7 +22,7 @@ module.exports = function (grunt) {
     // configurable paths
     var yeomanConfig = {
         app: 'app',
-        dist: 'dist'
+        dist: '../public' //'dist'
     };
 
     grunt.loadNpmTasks('grunt-connect-proxy');
